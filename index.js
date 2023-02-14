073eebb6f25d21f851fe97cf5b2d631e344aa6de
haruntasci <haruntasci77@gmail.com>
2023-02-15 00:00:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//Factory Function
+function createCircle(radius) {
+
+    return {
+        radius,
+        draw: function () {
+            console.log("draw")
+        }
+    }
+
+}
+
+const circle = createCircle(1)
+circle.draw()
